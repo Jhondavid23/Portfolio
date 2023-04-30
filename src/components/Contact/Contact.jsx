eb62578274d257ad640e8a93be55f1cf55f57c9b
Juan David <juandavidiazorozco@gmail.com>
2023-04-29 20:40:51 -0500

    avanzing on the proyect

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
 import { useState } from 'react';
 import './contact.scss'
+import sendEmail from './sendEmail';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 function Contact() {
     const [name, setName] = useState('');
     const [subject, setSubject] = useState('');
     const [message, setMessage] = useState('');
 
+    const form = useRef();
+
     const handleSubmit = async (event) => {
         event.preventDefault();
         const formData = { name, subject, message };
-        const response = await fetch('/send-email', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formData)
-        });
-        const data = await response.json();
-        console.log(data); // Maneja la respuesta del servidor
+
+        emailjs.sendForm('service_93kz5nq', 'template_g02qhy9', form.current, '58dds6zJWhzPpGzrP')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+
+        console.log({ name, subject, message }); // Maneja la respuesta del servidor
         setName('');
-        setEmail('');
         setSubject('');
         setMessage('');
     };
@@ -31,27 +36,30 @@ function Contact() {
                 <span>Email: juandaviddiazorozco@gmail.com</span>
             </div>
             <div className="c-left">
-                <form onSubmit={handleSubmit} className='c-form'>
+                <form ref={form} onSubmit={handleSubmit} className='c-form'>
                     <label>
                         <span>Name:</span>
                         <input
                             type="text"
                             value={name}
                             onChange={(event) => setName(event.target.value)}
+                            name='from_name'
                         />
                     </label>
                     <label>
-                        Asunto: 
+                        Asunto:
                         <input
                             type="text"
                             value={subject}
                             onChange={(event) => setSubject(event.target.value)}
+                            name='from_email'
                         />
                     </label>
                     <label>
                         Mensaje:
                         <textarea
                             value={message}
+                            name='message'
                             onChange={(event) => setMessage(event.target.value)}
                         />
                     </label>
